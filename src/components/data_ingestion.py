8930b55e80cab3acb4d936f1840f5db7002763dd
NvkAnirudh <nuti.krish4@gmail.com>
2023-11-13 23:43:02 -0500

    Added Data Transformations

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,14 +3,17 @@ import sys
 import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str = os.path.join('artifacts', "train.csv")
-    test_data_path: str = os.path.join('artifacts', "test.csv")
-    raw_data_path: str = os.path.join('artifacts', "raw_data.csv")
+    train_data_path: str = os.path.join('../../artifacts', "train.csv")
+    test_data_path: str = os.path.join('../../artifacts', "test.csv")
+    raw_data_path: str = os.path.join('../../artifacts', "raw_data.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -19,7 +22,9 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Started data ingestion component")
         try:
-            df = pd.read_csv("notebook/data/StudentsPerformance.csv")
+            df = pd.read_csv("../../notebook/data/StudentsPerformance.csv")  
+            df.rename(columns={'race/ethnicity':'race_ethnicity', 'parental level of education':'parental_level_of_education', 'test preparation course':'test_preparation_course','math score':'math_score',
+                   'reading score':'reading_score', 'writing score':'writing_score'}, inplace=True)          
             logging.info("Read the required data as pandas dataframe")
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -39,8 +44,12 @@ class DataIngestion:
                 self.ingestion_config.test_data_path
             )
         except Exception as e:
-            CustomException(e,sys)
+            raise CustomException(e,sys)
 
 if __name__=="__main__":
     di = DataIngestion()
-    di.initiate_data_ingestion()
+    train_path, test_path = di.initiate_data_ingestion()
+
+    # Performing DataTransformation
+    dt = DataTransformation()
+    train_arr, test_arr, _ = dt.initiate_data_transformation(train_path, test_path)
